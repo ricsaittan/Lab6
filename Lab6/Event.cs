2292d146bda03d17fdd62979612aa877cb38bcec
ricsaittan <151973832+ricsaittan@users.noreply.github.com>
2024-02-27 15:28:28 -0700

    Update Event.cs

--- Lab6/Event.cs
+++ Lab6/Event.cs
@@ -3,17 +3,21 @@ using System.Text.Json.Nodes;
 
 class Event
 {
+    //Setters and getters for EventNumber and Location
     public int EventNumber
     {get; set;}
     public string Location
     {get; set;}
 
+    //Serialization Method that creates the "event.json" file
     public void Serialization()
     {
         string jsonString = JsonSerializer.Serialize(this);
         File.WriteAllText("event.json", jsonString);
     }
 
+    //Deserialization Method that reads the Json file and creates the Event object again.
+    //Also reads the objects Properties
     public void DeSerialization()
     {
         string jsonString = File.ReadAllText("event.json");
@@ -22,6 +26,8 @@ class Event
         Console.WriteLine($"Location: {temp.Location}");
     }
 
+    //ReadFromFile Method that reads the "event.json" file while writes in it.
+    //Uses seek to get the bytes of the file and retranslates it back into char type.
     public void ReadFromFile()
     {
         using (StreamWriter write = new StreamWriter("event.json"))
